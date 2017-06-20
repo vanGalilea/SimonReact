e5c2023350b3cc0a4675861b2955e49ce29d31d5
vanGalilea <z.galili@icloud.com>
2017-06-20 16:06:12 +0200

    started making simonContainer and pads comp

---
+++ src/components/PadContainer.js
@@ -0,0 +1,30 @@
+// import React { PureComponent } from 'react'
+//
+// const sounds = [
+//   new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+//   new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+//   new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+//   new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
+// ]
+//
+// export class PadContainer extends PureComponent {
+//
+//
+//
+//   onClick = (item) =>{
+//        soundBoard[0].play();
+//     };
+//
+//     render() {
+//       var item = this.props.item
+//       return(
+//         <div>
+//           <Pad color="yellow" />
+//           <Pad color="blue" />
+//           <Pad color="red" />
+//           <Pad color="green" />
+//           <Control name='center' />
+//         </div>
+//       )
+//     }
+// }
