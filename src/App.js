17a98acb0e37cd96d19eb443a6759c141ba8337d
vanGalilea <z.galili@icloud.com>
2017-06-21 09:22:48 +0200

    created game page route, routing signup and in fixed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends Component {
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="App">
           <Navigation />
-          <SimonGame />
           { this.props.children }
         </div>
       </MuiThemeProvider>
